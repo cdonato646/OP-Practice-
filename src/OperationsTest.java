8783c21cd32f4bdc1d1945d3d88de6d88ab8c646
Dont Look Here <42975427+cdonato646@users.noreply.github.com>
2018-09-28 12:27:33 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,29 @@
+
+public class OperationsTest 
+	{
+	public static void main(String[] args) 
+		
+		{
+	int number; //Declaration statement
+	number = 20; //Assignment statement -> initialization
+	number = number + 5; // number+=5
+	System.out.println(number);
+
+		}	
+	
+		
+		
+		
+	
+
+
+	}
+
+
+
+
+
+
+
+
+
