a70e357502abeaf3e735dd2d8baa08ffa1ea1639
Dont Look Here <42975427+cdonato646@users.noreply.github.com>
2018-09-28 12:37:37 -0400

    Add files via upload

--- src/OperationsTest.java
+++ src/OperationsTest.java
@@ -6,9 +6,13 @@ public class OperationsTest
 		{
 	int number; //Declaration statement
 	number = 20; //Assignment statement -> initialization
+	int life;
+	life  = 15;
 	number = number + 5; // number+=5
 	System.out.println(number);
-
+	number+=life;
+	System.out.println(life);
+	System.out.println(number);
 		}	
 	
 		
